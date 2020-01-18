5794d30d4492666c3cc33d88a3c579ea1a0636f5
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-18 03:19:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,45 @@
-# Add your code here
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.save
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  #checkpoint
+  
+  
+ 
+  #prints name of each dog 
+  #iterate over all
+  #puts dog name property 
+  
+  def self.print_all
+    self.all.each do |dog| 
+      puts dog.name
+    end 
+  end 
+  
+   def save
+    self.class.all << self
+  end
+end
+
+# pluto = Dog.new("Pluto")
+# fido = Dog.new("Fido")
+# maddy = Dog.new("Maddy")
+
+
+# pluto
